a1dde54d0463b68aea5ff78f26c501a9520d4fba
SamAucoin <samuel.aucoin@mail.mcgill.ca>
2021-07-20 22:13:08 +0000

    Added file for sea ice data

---
+++ CDR Sea Ice Data.R
@@ -0,0 +1,13 @@
+library(ncdf4) # package for netcdf manipulation
+library(raster) # package for raster manipulation
+library(rgdal) # package for geospatial analysis
+library(ggplot2) # package for plotting
+
+filename <- ''
+
+nc_data <- nc_open("filename")
+
+lon <- ncvar_get(nc_data, "longitude")
+lat <- ncvar_get(nc_data, "latitude", verbose = F)
+t <- ncvar_get(nc_data, "time")
+sic <- ncvar_get(nc_data, 'cdr_seaice_conc')
